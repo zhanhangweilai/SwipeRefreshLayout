de7a35da72fdd65c25e5e00f23e061df6ee26d63
lihang <lihang@spreadst.com>
2018-05-08 17:47:12 +0800

    2018.5.8

---
+++ app/build/generated/source/buildConfig/debug/com/zyh/testswiperefresh/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.zyh.testswiperefresh;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.zyh.testswiperefresh";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
